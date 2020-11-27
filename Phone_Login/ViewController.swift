a217a31b3e375def2183b7dcc982c34a550e8b3a
spramani <shubhamramani903@gmail.com>
2020-11-27 16:47:16 +0530

    otpLogin

--- Phone_Login/ViewController.swift
+++ Phone_Login/ViewController.swift
@@ -6,14 +6,75 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var submitbtn: UIButton!
+    @IBOutlet weak var otp: UITextField!
+    @IBOutlet weak var phoneNumber: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        otp.isHidden = true
     }
+    
+    var verification_id : String? = nil
+    
+    @IBAction func submit(_ sender: UIButton) {
+        if (otp.isHidden) {
+            if !phoneNumber.text!.isEmpty {
+                Auth.auth().settings?.isAppVerificationDisabledForTesting = false
+                PhoneAuthProvider.provider().verifyPhoneNumber(phoneNumber.text!, uiDelegate: nil, completion: { (verificationId, error) in
+                    if(error != nil){
+                        return
+                    }
+                    else {
+                        
+                        self.verification_id = verificationId
+                        self.otp.isHidden = false
+                    }
+                })
+                
+            }else{
+                    print("Please Enter Phonenumber")
+                }
+                
+        }else{
+                if verification_id != nil {
+                let credential = PhoneAuthProvider.provider().credential(withVerificationID: verification_id!, verificationCode: otp.text!)
+                    Auth.auth().signIn(with: credential) { (authData, error) in
+                        if (error != nil ){
+                            let alertControllers = UIAlertController(title: "Error", message: error.debugDescription, preferredStyle: .alert)
+            
+                            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                            alertControllers.addAction(defaultAction)
+                            self.present(alertControllers, animated: true, completion: nil)
+                        }else {
+                            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                            
+                            //self.selectDeselect(tableview : tableView , indexpath : indexPath)
+                            
+                            let detail:secondViewController = storyboard.instantiateViewController(withIdentifier: "secondViewController") as! secondViewController
+                            
+                            self.navigationController?.pushViewController(detail, animated: true )
+                            
+                            
+                            
+                            print("Authentication Success With" + (authData?.user.phoneNumber ?? "No Phone Number"))
+                        }
+                        
+                    }
+                    
+                }else {
+                    print("Error in Getting verification code")
+                }
+            
+            }
+        }
+    }
+    
 
 
-}
+
+
 
---
+++ Phone_Login/secondViewController.swift
@@ -0,0 +1,29 @@
+//
+//  secondViewController.swift
+//  Phone_Login
+//
+//  Created by MAC on 27/11/20.
+//
+
+import UIKit
+
+class secondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
